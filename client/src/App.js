f3494c5db057eaebcf4d2544409dbe6ce38c14cc
rhonvyramos <rhonvyramos@gmail.com>
2023-10-20 09:32:31 -0400

    created Route element to route to Home page as landing page

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// imports Route as page router through URLs
+import { Route } from 'react-router-dom';
+
+// imports Home as landing page for the site
+// uses empty URL
+import { Home } from "./pages/Home.js"
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section id="page_container">
+          <Route path = "/" element={<Home />}></Route>
+        </section>
       </header>
     </div>
   );
