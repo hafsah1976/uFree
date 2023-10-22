d02bab242799ddb8080f17ebd631cf93a9adeb15
rhonvyramos <rhonvyramos@gmail.com>
2023-10-22 11:04:45 -0400

    added empty link to buttons; text decoration of buttons is none

--- client/src/components/HeaderNavBar/index.js
+++ client/src/components/HeaderNavBar/index.js
@@ -5,8 +5,8 @@ const HeaderNavBar = () => {
     return (
         <section id="content_header_nav">
             <section id="header_nav_buttons">
-                <div id="button_login" className="header_nav_button">Login</div>
-                <div id="button_createEvent"className="header_nav_button">Create Event</div>
+                <a href=""><div id="button_login" className="header_nav_button">Login</div></a>
+                <a href=""><div id="button_createEvent"className="header_nav_button">Create Event</div></a>
             </section>
         </section>
     )
