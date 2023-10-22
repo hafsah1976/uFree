0932e3b5966d00e1d7891c9e9cd84c0e66e553d2
rhonvyramos <rhonvyramos@gmail.com>
2023-10-22 11:09:48 -0400

    successful refactor to using Router instead of just page container

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 // imports Route as page router through URLs
-import { Routes, Route, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // imports Home as landing page for the site
 // uses empty URL
@@ -17,14 +17,25 @@ import HeaderNavBar from "./components/HeaderNavBar/index.js";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <section id="content_header">
-          <HeaderNavBar />
+      <Router>
+        <header className="App-header">
+          <section id="content_header">
+            <HeaderNavBar />
+          </section>
+        </header>
+        <section id="page_container">
+          <Routes>
+            <Route 
+            path='/'
+            element={<Home />}
+            />
+            <Route 
+            path='/:pathName'
+            element={<PageContainer />}
+            />
+          </Routes>
         </section>
-      </header>
-      <section id="page_container">
-        <PageContainer />
-      </section>
+      </Router>
     </div>
   );
 }
