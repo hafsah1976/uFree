622f40dbd3aa214bd1c6259da95b44e8555828e0
rhonvyramos <rhonvyramos@gmail.com>
2023-10-22 11:15:18 -0400

    created buttons and intro page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,14 @@ const Home = () => {
 
     return (
         <section id="content_home_page">
-            
+            <section id='home_page_title'>uFree</section>
+            <section id='home_page_intro'>Get Togethers, minus the hassle</section>
+            <section id='home_page_buttons'>
+                <div>
+                    <a href=""><div id="button_login" className="homepage_button">Login</div></a>
+                    <a href=""><div id="button_createEvent"className="homepage_button">Create Event</div></a>
+                </div>
+            </section>
         </section>
     );
 };
