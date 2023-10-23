2aa9018637747bdb8d0ccecb6e137865bc7dccf9
rhonvyramos <rhonvyramos@gmail.com>
2023-10-23 19:20:33 -0400

    styled dashboard header buttons

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -13,21 +13,26 @@ const Dashboard = () => {
                 <div id="header_content">
                     <h1 id='header_title'>Dashboard</h1>
                     <div id="header_buttons">
-                        <Link to="/createEventPage">
-                            <div id="dashboard_createEvent_button">Create Event</div>
+                        <Link className='style_link' to="/createEventPage">
+                            <div id="dashboard_createEvent_button" className='dashboard_button'>Create Event</div>
                         </Link>
-                        <Link to="/joinEventPage">
-                            <div id="dashboard_joinEvent_button">Join Event</div>
+                        <Link className='style_link' to="/joinEventPage">
+                            <div id="dashboard_joinEvent_button" className='dashboard_button'>Join Event</div>
                         </Link>
                     </div>
                 </div>
                 <div id="dashboard_event_board">
-                    <div id='board_title'>
+                    <h2 id='board_title'>
                         Events
-                    </div>
+                    </h2>
                     <div id='board_elements'>
                         <div className='event_box'>
-
+                            <div id='event_header'>
+                                <h3>An Event</h3>
+                            </div>
+                            <div id="event_footer">
+                                <p>An event's description</p>
+                            </div>
                         </div>
                         <div className='event_box'>
                             
@@ -42,7 +47,12 @@ const Dashboard = () => {
                             
                         </div>
                         <div className='event_box'>
-                            
+                            <div id='event_header'>
+                                <h3>An Event</h3>
+                            </div>
+                            <div id="event_footer">
+                                <p>An event's description</p>
+                            </div>
                         </div>
                         <div className='event_box'>
                             
