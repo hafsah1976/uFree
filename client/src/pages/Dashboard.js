7bde362ef2a8247df8c85392f6577155061a6873
rhonvyramos <rhonvyramos@gmail.com>
2023-10-23 18:54:07 -0400

    creating Dashboard elements; created dashboard css file; added basic format styling

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "../assets/dashboard.css";
 
 const Dashboard = () => {
 
@@ -7,6 +9,20 @@ const Dashboard = () => {
             <p id="DEBUG_dashboard_page_text" className='DEBUG_text'>
                 PLACEHOLDER DASHBOARD TEXT
             </p>
+            <section id="dashboard_header">
+                <div id="header_content">
+                    <h1 id='header_title'>Dashboard</h1>
+                    <div id="header_buttons">
+                        <Link to="/createEventPage">
+                            <div id="dashboard_createEvent_button">Create Event</div>
+                        </Link>
+                        <Link to="/joinEventPage">
+                            <div id="dashboard_joinEvent_button">Join Event</div>
+                        </Link>
+                    </div>
+                </div>
+                <div id="header_event_board"></div>
+            </section>
         </section>
     );
 };
