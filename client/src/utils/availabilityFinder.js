9b3ef09be3d5e9044be12909182039a0167e24de
Jacob Medina <jacobmedina1618@gmail.com>
2023-10-24 03:19:10 -0400

    adjusted algorithm to work with new model design

--- client/src/utils/availabilityFinder.js
+++ client/src/utils/availabilityFinder.js
@@ -1,3 +1,7 @@
+function isNotAvailable(avail) {
+    return (avail.start === 0 && avail.end === 0);
+}
+
 /* 
 userAvails -> array of user availability objects for a given day
 {
@@ -10,7 +14,7 @@ userAvails -> array of user availability objects for a given day
 // returns an availability object with a start and end time (or null)
 function findAvailability(userAvails) {
     // if any user is not available, return null
-    if (userAvails.includes(null)) return null;
+    if (userAvails.some(a => isNotAvailable(a))) return null;
 
     // array of all start times
     const startTimes = userAvails.map(userAvail => userAvail.start);
@@ -33,33 +37,61 @@ function findAvailability(userAvails) {
 
 // returns an array of availability objects
 function getDayAvailabilities(userWeekAvails) {
-    return userWeekAvails.map(userAvails => findAvailability(userAvails))
+    return userWeekAvails.map(userAvails => {
+        return {
+            [userAvails[0].day]: findAvailability(userAvails)
+        }
+    })
 }
 
 // have an array of user week avails containing avails for each day the week for that user
 // need an array of days containing each user avail for that day
 function getUserWeekAvails(availabilitiesSchemaArray) {
     const avails = availabilitiesSchemaArray;
-    const days = new Set(availabilitiesSchemaArray.map(a => a.map(b => b.day)))
+    const daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+    const userWeekAvails = [];
+    for (const day of daysOfWeek) {
+        userWeekAvails.push(avails.map(user => 
+            user.availabilities.find(a => a.day === day)
+        ))
+    }
+
+    return userWeekAvails;
 }
 
-// function test(args) {
-//     console.log(getDayAvailabilities(args));
-// }
+function test(args) {
+    const weekAvails = getUserWeekAvails(args);
+    console.log(JSON.stringify(getUserWeekAvails(args), null, 4));
+    console.log(getDayAvailabilities(weekAvails));
+    // console.log();
+}
 
 // test([
-//     [
-//         { start: 10, end: 20.5 },
-//         { start: 12, end: 17 },
-//         { start: 13, end: 24 },
-//         { start: 15, end: 22.5 },
-//     ],
-//     [
-//         { start: 17, end: 24 },
-//         { start: 10, end: 18 },
-//         { start: 15, end: 24 },
-//         { start: 15, end: 22.5 },
-//     ],
+//     {
+//         userId: '123456',
+//         availabilities: [
+//             { day: 'monday', start: 10, end: 20.5 },
+//             { day: 'tuesday', start: 12, end: 17 },
+//             { day: 'wednesday', start: 13, end: 24 },
+//             { day: 'thursday', start: 15, end: 22.5 },
+//             { day: 'friday', start: 15, end: 22.5 },
+//             { day: 'saturday', start: 15, end: 22.5 },
+//             { day: 'sunday', start: 15, end: 22.5 },
+//         ]
+//     },
+//     {
+//         userId: 'iusdbv832989',
+//         availabilities: [
+//             { day: 'monday', start: 0, end: 12 },
+//             { day: 'tuesday', start: 12, end: 17 },
+//             { day: 'wednesday', start: 13, end: 24 },
+//             { day: 'thursday', start: 15, end: 22.5 },
+//             { day: 'friday', start: 15, end: 22.5 },
+//             { day: 'saturday', start: 15, end: 22.5 },
+//             { day: 'sunday', start: 15, end: 22.5 },
+//         ]
+//     },
 // ])
 
-export { getDayAvailabilities };
+// export { getDayAvailabilities };
