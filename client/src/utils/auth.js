a270948c95428d7572211ebb8a41cd4de4c25f55
Khan-HB <hafsah999@gmail.com>
2023-10-23 20:26:35 -0400

    added function loggedin() to check if the user is logged in  to client/src/auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -7,5 +7,9 @@ class AuthService {
   getProfile() {
     return decode(this.getToken());
   }
-
-  
+    // Check if the user is logged in
+    loggedIn() {
+        // Checks if there is a saved token and it's still valid
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token); // this line is focusing on the core concept of checking for the presence of a valid token and token expiration
+      }
