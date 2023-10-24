0df0452702f8c09345d06d2f1b6891d75a3d0418
rhonvyramos <rhonvyramos@gmail.com>
2023-10-23 20:35:44 -0400

    react form input for event name implemented

--- client/src/pages/CreateEvent.js
+++ client/src/pages/CreateEvent.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../assets/createEvent.css"
 
 const CreateEvent = () => {
+    const [eventInputs, setEventInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setEventInputs(values => ({...values, [name]: value }))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(eventInputs);
+    }
 
     return (
         <section id="content_createEvent_page">
@@ -13,22 +25,28 @@ const CreateEvent = () => {
                 <h1>Create an Event</h1>
             </div>
             <div id="createEvent_form">
-                <form>
-                        <input type='text' id="event_name" placeholder='Name your event...' />
-                        <input type='text' id="event_week" placeholder='Week of your event...' />
-                        <input type='text' id="event_location" placeholder='Location of your event...' />
-                        <input type='text' id="event_description" placeholder='Description of your event...' />         
+                <form onSubmit={handleSubmit}>
+                    <input 
+                    type='text' 
+                    id="event_name"
+                    name='event_name' 
+                    placeholder='Name your event...'
+                    value={eventInputs.eventName} 
+                    onChange={handleChange} />
+                    <input type='text' id="event_week" placeholder='Week of your event...' />
+                    <input type='text' id="event_location" placeholder='Location of your event...' />
+                    <input type='text' id="event_description" placeholder='Description of your event...' />         
 
-                        <select id='event_thumbnail'>
-                            <option value="thumbnail_tools">Tools</option>
-                            <option value="thumbnail_party">Party</option>
-                            <option value="thumbnail_celebration">Celebration</option>
-                            <option value="thumbnail_scenery">Scenery</option>
-                        </select>
+                    <select id='event_thumbnail'>
+                        <option value="thumbnail_tools">Tools</option>
+                        <option value="thumbnail_party">Party</option>
+                        <option value="thumbnail_celebration">Celebration</option>
+                        <option value="thumbnail_scenery">Scenery</option>
+                    </select>
                 
-                        <input type='submit' value="submit" />           
-                    </form>
-                </div>
+                    <input type='submit' value="submit" />           
+                </form>
+            </div>
         </section>
     );
 };
