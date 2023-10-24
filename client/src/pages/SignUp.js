c2fe8cce62d85a8b4984eefe4e84a12248b848c4
Khan-HB <hafsah999@gmail.com>
2023-10-23 22:07:34 -0400

    added the signup form that takes in username, email, and password.

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useState } from 'react'; // Importing the useState hook
 import { Link, useNavigate } from 'react-router-dom'; // Importing the Link component and useNavigate hook
-import Auth from '../utils/auth'; // Importing the Auth utility (likely for user authentication)
+import Auth from '../utils/auth'; // Importing the Auth utility
 
 // Defining the Signup component/page
-export default  Signup = () => {
+const Signup = () => {
 
   // Initializing state using the useState hook
   const [data, setData] = useState({
@@ -19,4 +19,44 @@ export default  Signup = () => {
   // Obtaining a navigation function using the useNavigate hook
   const navigate = useNavigate();
 
+  // Rendering of the Signup component
+  return (
+    <>
+      <div>
+        {/* The Signup form */}
+        <form>
+          {/* Input field for the username */}
+          <input
+            type="text"
+            placeholder="Create your Username"
+            name="username"
+            value={data.username}
+            // Add validation to check if the username is taken
+          />
+
+          {/* Input field for the email address */}
+          <input 
+            type="email"
+            placeholder="Your Email"
+            name="email"
+            value={data.email}
+            // add validation to check if the user with this email already exists
+          />
+
+          {/* Input field for the password */}
+          <input
+            type="password"
+            placeholder="Create Your Password"
+            name="password"
+            value={data.password}
+            onChange={(e) => {setData({...data, password: e.target.value})}}
+            // Add an event handler (onChange) to update the password in the state, 
+            //validation for password exists in the user model..??
+          />
+</form>
+      </div>
+    </>
+  );
 }
+
+export default Signup; // Export the Signup component
