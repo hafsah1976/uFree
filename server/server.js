4f667f305b03be30e36d0aca2e92263a9b4eb349
Ibrahima <iab.jr10@gmail.com>
2023-10-25 05:15:12 -0400

    Uncomment code in server.js

--- server/server.js
+++ server/server.js
@@ -8,11 +8,11 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: authMiddleware,
-// });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -28,16 +28,16 @@ app.get('/', (req, res) => {
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
-//   await server.start();
-//   server.applyMiddleware({ app });
-  
+  await server.start();
+  server.applyMiddleware({ app });
+
   db.once('open', () => {
     app.listen(PORT, async () => {
       console.log(`API server running on port ${PORT}!`);
-    //   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
   };
-  
+
 // Call the async function to start the server
 startApolloServer();
