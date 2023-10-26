39612fe3868f4ba771e99e6c961f8150b0808299
Jacob Medina <jacobmedina1618@gmail.com>
2023-10-25 21:39:10 -0400

    added apollo client provider to app

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './helpers.css';
 import './App.css';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 // importing normalize.css to normalize page element styling
 import './normalize.css'
 
@@ -20,49 +28,75 @@ import HeaderNavBar from "./components/HeaderNavBar/index.js";
 
 import { Login, Event, Dashboard, SignUp, Home, Availabilities, CreateEvent } from "./pages/PageContainer.js";
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <header className="App-header">
-          <section id="content_header">
-            <HeaderNavBar />
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Router>
+          <header className="App-header">
+            <section id="content_header">
+              <HeaderNavBar />
+            </section>
+          </header>
+          <section id="page_container">
+            <Routes>
+              <Route 
+              path='/'
+              element={<Home />}
+              />
+              <Route 
+              path='/login'
+              element={<Login />}
+              />
+              <Route 
+              path='/dashboard'
+              element={<Dashboard />}
+              />
+              <Route 
+              path='/eventPage'
+              element={<Event />}
+              />
+              <Route 
+              path='/events/create'
+              element={<CreateEvent />}
+              />
+              <Route 
+              path='/signup'
+              element={<SignUp />}
+              />
+              <Route 
+              path='events/:eventId?/availabilities'
+              element={<Availabilities />}
+              />              
+            </Routes>
           </section>
-        </header>
-        <section id="page_container">
-          <Routes>
-            <Route 
-            path='/'
-            element={<Home />}
-            />
-            <Route 
-            path='/login'
-            element={<Login />}
-            />
-            <Route 
-            path='/dashboard'
-            element={<Dashboard />}
-            />
-            <Route 
-            path='/eventPage'
-            element={<Event />}
-            />
-            <Route 
-            path='/events/create'
-            element={<CreateEvent />}
-            />
-            <Route 
-            path='/signup'
-            element={<SignUp />}
-            />
-            <Route 
-            path='events/:eventId?/availabilities'
-            element={<Availabilities />}
-            />              
-          </Routes>
-        </section>
-      </Router>
-    </div>
+        </Router>
+      </div>
+    </ApolloProvider>
   );
 }
 
