983bf7c1a363a1894a080c75401367186153ab63
Jacob Medina <jacobmedina1618@gmail.com>
2023-10-28 04:55:03 -0400

    added log out and brand to navbar

--- client/src/components/HeaderNavBar/index.js
+++ client/src/components/HeaderNavBar/index.js
@@ -11,9 +11,10 @@ const HeaderNavBar = ({ logoutFunc, loggedIn }) => {
     return (
         <section id="content_header_nav">
             <section id="header_nav_buttons">
-                <Link to="/"><div id="DEBUG_button_homepage" className="header_nav_button">Home</div></Link>
-                <Link to="/login"><div id="button_login" className="header_nav_button">Login</div></Link>
-                <Link to="/signup"><div id="button_login" className="header_nav_button">Sign Up</div></Link>
+                <Link to="/" className="nav_brand">uFree</Link>
+                {/* <Link to="/"><div id="DEBUG_button_homepage" className="header_nav_button">Home</div></Link> */}
+                {/* <Link to="/login"><div id="button_login" className="header_nav_button">Login</div></Link> */}
+                {/* <Link to="/signup"><div id="button_login" className="header_nav_button">Sign Up</div></Link> */}
                 <Link to="/dashboard"><div id="DEBUG_button_dashboard" className="header_nav_button">Dashboard</div></Link>
                 <Link to="/events/create"><div id="button_createEvent"className="header_nav_button">Create Event</div></Link>
                 <Link to="/events"><div id="DEBUG_button_eventPage" className="header_nav_button">Search Event</div></Link>
