9fb67ea577dc83afcc88e71d87863fc95d238a98
Jacob Medina <jacobmedina1618@gmail.com>
2023-10-28 03:37:15 -0400

    Add availabilities page side image

--- client/src/images/page-images/index.js
+++ client/src/images/page-images/index.js
@@ -1,9 +1,12 @@
-import home from './home-page.png';
+// import home from './home-page.png';
+import home from './friend-group.png';
 import createEvent from './create-event.jpg';
+import addAvailabilities from './add-availabilities.png';
 
 const pageImages = {
     home,
     createEvent,
+    addAvailabilities,
 }
 
 export default pageImages;
