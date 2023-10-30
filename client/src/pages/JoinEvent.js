16b5e9576ed9cc012c1d7fa67f24e20608c5c771
rhonvyramos <rhonvyramos@gmail.com>
2023-10-30 10:23:27 -0400

    join event use state and handle change and submits implemented

--- client/src/pages/JoinEvent.js
+++ client/src/pages/JoinEvent.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const JoinEvent = () => {
+
+    const [joinEventInput, setJoinEventInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const event_code = event.target.value;
+        setJoinEventInputs(values => ({...values, [name]: event_code}))
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(joinEventInput);
+    };
+
     return(
         <section id='content_joinEvent_page'>
             <p id="DEBUG_joinEvent_page_text" className='DEBUG_text'>
                 PLACEHOLDER JOIN EVENT TEXT
             </p>
+            <section id="joinEvent_form">
+                <form>
+
+                </form>
+            </section>
         </section>
     );
 };
