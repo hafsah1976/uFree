bdc57707247b8560ba33aa8c3c9d7c81d93eb9c4
Ibrahima <iab.jr10@gmail.com>
2023-10-29 22:24:51 -0400

    Add EditAvailabiliteis file Import EditAvails from EditAvailabilities file Add EditAvails route to App.js

--- client/src/pages/PageContainer.js
+++ client/src/pages/PageContainer.js
@@ -4,7 +4,8 @@ import Dashboard from './Dashboard';
 import SignUp from './SignUp';
 import Home from './Home';
 import Availabilities from './AddAvailabilities';
+import EditAvails from './EditAvailabilities';
 import CreateEvent from './CreateEvent';
 import GoToEvent from './GoToEvent';
 
-export { Login, Event, Dashboard, SignUp, Home, Availabilities, CreateEvent, GoToEvent };
+export { Login, Event, Dashboard, SignUp, Home, Availabilities, EditAvails, CreateEvent, GoToEvent };
